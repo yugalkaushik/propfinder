4a7dfd76e7c2a99c37b6bd4c8a81576ef65af432
Yugal Kaushik <yugalkaushikdev@gmail.com>
2026-09-17 19:53:32 +0000

    Initial project setup

---
+++ backend/src/main/java/com/propfinder/PropFinderApplication.java
@@ -0,0 +1,12 @@
+package com.propfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropFinderApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/propfinder/PropFinderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.propfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
